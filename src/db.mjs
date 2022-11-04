8d65adaa2f7e89dc07d2285af3487b31ead65029
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-11-04 11:12:51 +0100

    feat: declare name in class as unique

--- src/db.mjs
+++ src/db.mjs
@@ -14,6 +14,7 @@ export function makeDb() {
             local: "superId",
             ref: "Class.id"
         })
+        .addUnique('uq_name', ['name'])
 
     schemaBuilder.createTable("Property")
         .addColumn("id", lf.Type.INTEGER)
